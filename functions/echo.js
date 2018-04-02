9cf78d05c14e942b7796c64bb28d26251e0f0c02
Jykinturah Sparkle <jykinturah@gmail.com>
2018-04-02 00:42:14 -0700

    targetted echo

--- functions/echo.js
+++ functions/echo.js
@@ -13,10 +13,14 @@ module.exports = {
   },
   execute: function(bot, args, message) {
     if(message.content.startsWith("#echo")){
-      say = message.content.substring(5,message.length);
-      message.channel.send(say);
-      message.delete()
-        .catch(console.error);
+      let targetChannel = message.guild.channels.get(message.content.substring(5 + 1 + 2, 5 + 1 + 2 + 18));
+      console.log(message.content.substring(5 + 1 + 2, 5 + 1 + 2 + 18));
+      if(targetChannel){
+        targetChannel.send(message.content.substring(5 + 1 + 21)).catch(console.error);
+      } else {
+        message.channel.send(message.content.substring(5)).catch(console.error);
+        message.delete().catch(console.error);
+      }
     }
   }
 }
